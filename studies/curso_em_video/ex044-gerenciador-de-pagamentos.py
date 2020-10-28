fe4261b11e71c51181680b3e7a2df181cca7d404
alexander-colaneri <alexcol@gmail.com>
2020-10-28 11:33:46 -0300

    Update ex044-gerenciador-de-pagamentos.py
    Transformado para classe e funções.

--- studies/curso_em_video/ex044-gerenciador-de-pagamentos.py
+++ studies/curso_em_video/ex044-gerenciador-de-pagamentos.py
@@ -4,42 +4,75 @@
 # - à vista no cartão: 5% de desconto
 # - em até 2x no cartão: preço formal
 # - 3x ou mais no cartão: 20% de juros
-print()
-print(f'{" LOJAS POLENTÃO ":*^45}')
-print()
-preço = float(input('Digite o preço do produto ou serviço: '))
-print()
-print('''As opções de pagamento são:
-[ 1 ] - À vista em dinheiro/cheque.
-[ 2 ] - À vista no cartão.
-[ 3 ] - Em 2x no cartão.
-[ 4 ] - Em 3x ou mais no cartão.''')
-print()
-opção = int(input('Digite a opção selecionada acima: '))
-if opção == 1:
-    desconto = preço * 10 / 100
-    total = preço - desconto
-    print(f'''* Desconto de R${desconto:.2f}
-* TOTAL: R${total:.2f}''')
-elif opção == 2:
-    desconto = preço * 5 / 100
-    total = preço - desconto
-    print(f'''* Desconto: R${desconto:.2f}
-* TOTAL: R${total:.2f}.''')
-elif opção == 3:
-    parcela = preço / 2
-    print(f'''* 2 parcelas sem juros de R${parcela:.2f}
-* TOTAL: R${preço:.2f}''')
-elif opção == 4:
-    parcelas = int(input('Digite a quantidade de parcelas: '))
-    print()
-    if parcelas <= 2:
-        print('Opção inválida. Escolha uma opção a partir de 3 parcelas.')
-    elif parcelas >= 3:
-        juros = preço * 20 / 100
-        total = preço + juros
-        valorparcela = total / parcelas
-        print(f'''Opção com parcelamento (3x ou mais), COM JUROS:
-* Juros (20%): R${juros:.2f}
-* {parcelas}x de R${valorparcela:.2f}
-* TOTAL: De R${preço:.2f} para R${total:.2f}''')
+
+class GerenciadorPagamentos():
+    '''Calculadora de pagamentos, de acordo com a forma de pagamento.'''
+    desconto_vista = 0.1
+    desconto_cartao = 0.05
+    juros_parcelado = 0.2
+
+    def __init__(self):
+        self.menu = '''\nAs opções de pagamento são:
+    [ 0 ] - À vista em dinheiro/cheque.
+    [ 1 ] - À vista no cartão.
+    [ 2 ] - Em 2x no cartão.
+    [ 3 ] - Em 3x ou mais no cartão.\n
+Digite a opção selecionada:'''
+        self.valor = 0
+        self.desconto_10 = 0
+        self.desconto_5 = 0
+        self.parcelas = 0
+
+    def iniciar(self):
+        '''Menu principal.'''
+        print(f'{" SUPER LOJA ":*^40}')
+        self.digitar_valor()
+        print(self.menu)
+
+        opcao = input()
+        if opcao == '0':
+            self.pagar_a_vista()
+        elif opcao == '1':
+            self.pagar_cartao()
+        elif opcao == '2':
+            self.pagar_2x()
+        elif opcao == '3':
+            self.pagar_3x_mais()
+        else:
+            print('Opção inválida!')
+
+    def digitar_valor(self):
+        '''Valor do produto a ser comprado.'''
+        print('\nDigite o valor do produto:')
+        self.valor = float(input())
+
+    def pagar_a_vista(self):
+        '''Calcula valor para pagamento à vista, com 10% de desconto.'''
+        desconto = gerenciador.desconto_vista
+        self.desconto_10 = self.valor * desconto
+        total = self.valor - self.desconto_10
+        print(f'\nDesconto de 10%: R${self.desconto_10:.2f}\nTOTAL: R${total:.2f}')
+    
+    def pagar_cartao(self):
+        '''Calcula valor para pagamento à vista, com 5% de desconto.'''
+        desconto = gerenciador.desconto_cartao
+        self.desconto_5 = self.valor * desconto
+        total = self.valor - self.desconto_5
+        print(f'\nDesconto de 5%: R${self.desconto_5:.2f}\nTOTAL: R${total:.2f}')
+
+    def pagar_2x(self):
+        '''Calcula valor para pagamento em 2 parcelas iguais, sem desconto.'''
+        parcela = self.valor / 2
+        print(f'\nDuas parcelas iguais de R${parcela:.2f}')
+
+    def pagar_3x_mais(self):
+        '''Calcula valor para 3 ou mais parcelas, com 20% de juros no preço.'''
+        print('Digite o total de parcelas: ')
+        self.parcelas = int(input())
+        juros = gerenciador.juros_parcelado
+        parcela = (self.valor + (self.valor * juros)) / self.parcelas
+        print(f'\n{self.parcelas} parcelas de R${parcela:.2f}\nTOTAL: R${parcela * self.parcelas:.2f}')
+
+
+gerenciador = GerenciadorPagamentos()
+gerenciador.iniciar()
