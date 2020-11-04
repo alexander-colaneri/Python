688ee2e57a4e7f8bc82b43dd4e00711af7621703
alexander-colaneri <alexcol@gmail.com>
2020-11-04 19:57:00 -0300

    Update ex010-conversor-de-moedas.py
    Conversão para classe e funções.

--- studies/curso_em_video/ex010-conversor-de-moedas.py
+++ studies/curso_em_video/ex010-conversor-de-moedas.py
@@ -1,6 +1,20 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+# Descrição: Crie um programa que leia quanto dinheiro uma pessoa tem na 
+# carteira e mostre quantos dólares ela pode comprar.
 
-print('-' * 10, 'Conversor de Real em Dólar', '-'*10)
-real = float(input('Quantos Reais? R$'))
-dólar = real / 5.86
-print(f'R${real:.2f} corresponde a US${dólar:.2f}')
+class ConversorDolares():
+    '''Converte o valor em Reais para Dólares. US$ 1 == R$ 5.76 em Nov/2020'''
+    dolar = 5.76
+
+    def __init__(self):
+        self.reais = 0
+
+    def converter_reais_para_dolares(self):
+        '''Converte e exibe o resultado.'''
+        print('Indique o valor em Reais:')
+        self.reais = float(input())
+        valor = self.reais / conversor.dolar
+        print(f'Valor em Dólares: US${valor:.2f}')
+
+
+conversor = ConversorDolares()
+conversor.converter_reais_para_dolares()
