59e4abf2bceb6cb55e2999433f4feb4d77346878
alexander-colaneri <alexcol@gmail.com>
2020-11-11 20:27:56 -0300

    Update ex036-inicio-mundo-2-aprovando-emprestimo.py
    Convertido em classe e funções, e inclusão de manipulação de exceções.

--- studies/curso_em_video/ex036-inicio-mundo-2-aprovando-emprestimo.py
+++ studies/curso_em_video/ex036-inicio-mundo-2-aprovando-emprestimo.py
@@ -1,22 +1,78 @@
-print()
-# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa,
-# o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então
-# o empréstimo será negado.
-from emoji import emojize
+'''Aprovando Empréstimo
+Descrição: Escreva um programa para aprovar o empréstimo bancário para a compra de
+uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele
+vai pagar. A prestação mensal não pode exceder 30% do salário ou então o
+empréstimo será negado.'''
 
-print(emojize(':house_with_garden: Calculadora de Prestação para Compra de Casa :house_with_garden:', use_aliases=True))
-print()
-casa = float(input('Digite o valor da casa: R$'))
-salário = float(input('Digite o valor do salário: R$'))
-anos = int(input('Digite a quantidade de anos para pagamento: '))
-print()
-meses = anos * 12
-prestação = casa / meses
-if prestação >= salário * 30 / 100:
-    print(f'O valor da prestação é R${prestação:.2f}, com pagamento em {meses} meses.'
-          f'\nO valor permitido de prestação, de acordo com o salário, é de até R${salário * 30 / 100:.2f}.'
-          f'\n\033[1;31mPagamento não aprovado.\033[m')
-else:
-    print(f'O valor da prestação é R${prestação:.2f}, com pagamento em {meses} meses.'
-          f'\nO valor permitido de prestação, de acordo com o salário, é de até R${salário * 30 / 100}.'
-          f'\n\033[1;32mPagamento aprovado!\033[m')
+class AnalisadorDeEmprestimo():
+    '''Analisa se, de acordo com o salário, empréstimo para compra de casa é 
+    liberado. Prestação mensal não pode ultrapassar 30% do salário.'''
+    porcentagem_de_aprovacao = 0.3
+
+    def __init__(self):
+        self.valor_casa = 0
+        self.salario = 0
+        self.meses_para_pagamento = 0
+        self.valor_parcela = 0
+
+    def iniciar(self):
+        print(f'{" ANALISADOR DE EMPRÉSTIMO PARA COMPRA DE CASA ":*^60}')
+        self.receber_informacoes()
+        self.calcular_prestacao()
+        self.analisar_viabilidade()
+        self.indicar_resultado()
+
+    def receber_informacoes(self):
+        '''Recebe valor da casa, salário do comprador e anos para pagamento.'''
+        print('Digite o valor da casa:')
+        while True:
+            try:
+                self.valor_casa = float(input())
+            except ValueError:
+                print('Digite apenas números, sem espaços.')
+            else:
+                break
+        print('Digite o valor do seu rendimento mensal:')
+        while True:
+            try:
+                self.salario = float(input())
+            except:
+                print('Digite apens números, sem espaços.')
+            else:
+                break
+        print('Digite a quantidade de anos para pagamento.')
+        while True:
+            try:
+                anos_para_pagamento = int(input())
+            except:
+                print('Digite apenas números, sem espaços.')
+            else:
+                self.meses_para_pagamento = anos_para_pagamento * 12
+                break
+        return None
+
+    def calcular_prestacao(self):
+        '''Calcula vaor da casa pela quantidade de meses de pagamento.'''
+        self.valor_parcela = self.valor_casa / self.meses_para_pagamento
+        return None
+
+    def analisar_viabilidade(self):
+        '''Liberar o empréstimo caso valor da mensalidade não ultrapasse 30% do valor do salário.'''
+        if self.valor_parcela <= self.salario * analisador.porcentagem_de_aprovacao:
+            return True
+        else:
+            return False
+    
+    def indicar_resultado(self):
+        if self.analisar_viabilidade():
+            print(f'\nO valor da prestação é de {self.meses_para_pagamento} vezes de R${self.valor_parcela:.2f}')
+            print(f'O valor de 30% de seu salário é R${self.salario * analisador.porcentagem_de_aprovacao:.2f}')
+            print(f'Empréstimo APROVADO!')
+        else:
+            print(f'\nO valor da prestação é de {self.meses_para_pagamento} vezes de R${self.valor_parcela:.2f}')
+            print(f'O valor de 30% de seu salário é R${self.salario * analisador.porcentagem_de_aprovacao:.2f}')
+            print(f'Empréstimo REPROVADO!')
+
+
+analisador = AnalisadorDeEmprestimo()
+analisador.iniciar()
