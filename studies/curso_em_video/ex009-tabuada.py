b25a72e99b9f44244743befc6e44d89a9179952d
alexander-colaneri <alexcol@gmail.com>
2020-11-14 22:00:14 -0300

    Update ex009-tabuada.py
    Convertido em classe e funções, com manipulação de exceções.

--- studies/curso_em_video/ex009-tabuada.py
+++ studies/curso_em_video/ex009-tabuada.py
@@ -1,17 +1,45 @@
-# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+# Nº 9 - Tabuada
+# Descrição: Faça um programa que leia um número Inteiro qualquer e mostre na tela a
+# sua tabuada.
 
-print('-' * 10, 'Tabuada', '-' * 10)
-print()
-n = int(input('Digite um número para ver sua tabuada: '))
-print('-' * 25)
-print(f'{n} x {1 : 2} = {n * 1}\n'
-      f'{n} x {2 : 2} = {n * 2}\n'
-      f'{n} x {3 : 2} = {n * 3}\n'
-      f'{n} x {4 : 2} = {n * 4}\n'
-      f'{n} x {5 : 2} = {n * 5}\n'
-      f'{n} x {6 : 2} = {n * 6}\n'
-      f'{n} x {7 : 2} = {n * 7}\n'
-      f'{n} x {8 : 2} = {n * 8}\n'
-      f'{n} x {9 : 2} = {n * 9}\n'
-      f'{n} x {10 : >2} = {n * 10}')
-print('-' * 25)
+class CalculadoraDeTabuada():
+    '''Calcula um número indicado pelo usuário multiplicado de 1 a 10.'''
+    def __init__(self):
+        self.numero = 0
+        self.resultados = []
+
+    def iniciar(self):
+        '''Função inicial do programa.'''
+        print(f'{" CALCULADORA DE TABUADA ":*^40}')
+        self.receber_numero()
+        self.calcular_tabuada()
+        self.mostrar_resultados()
+        print('\nTenha um bom dia!')
+
+    def receber_numero(self):
+        '''Recebe número indicado pelo usuário para cálculo de tabuada.'''
+        print('Indique o número que deseja calcular a tabuada:')
+        while True:
+            try:
+                self.numero = int(input())
+                break
+            except ValueError:
+                print('Digite um número inteiro, sem espaços ou letras.')
+
+    def calcular_tabuada(self):
+        '''Calcula a tabuada de um número indicado pelo usuário.'''
+        for i in range(1, 11):
+            self.resultados.append(self.numero * i)  # Resultados armazenados em uma lista.
+        return None
+
+    def mostrar_resultados(self):
+        '''Exibe os cálculos e resultados.'''
+        print('-' * 20)
+        for i in range(1, 11):
+            print(f'{self.numero} X {i : >2} = {self.resultados[i - 1]}')
+        print('-' * 20)
+        return None
+
+
+tabuada = CalculadoraDeTabuada()
+tabuada.iniciar()
