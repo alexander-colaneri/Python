16d1bcd7da8886c643116f503a45d611e2346705
alexander-colaneri <alexcol@gmail.com>
2020-11-17 18:35:01 -0300

    Update ex004-dissecando-uma-variavel.py
    Transformado em funções. Melhoras na interface.

--- studies/curso_em_video/ex004-dissecando-uma-variavel.py
+++ studies/curso_em_video/ex004-dissecando-uma-variavel.py
@@ -1,21 +1,29 @@
+# Exercício 4:
 # Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis
 # sobre ele.
 
-# Obs: o que vem antes de um . , é chamado de "objeto". Ou seja, em "a.isnumeric", o "a" é um objeto e o "isnumeric"
-# método (por que tem () no final)
+def receber_caracteres():
+    '''Recebe números ou strings para serem analisados'''
+    print(f'{" ANALISADOR DE CARACTERES ":=^40}')
+    print('Digite algo (palavra ou números) para ser analisado:')
+    a = input()
+    return a
 
-a = input('Digite algo: ')
-print('O tipo primitivo desse valor é', type(a))
-print('É um número?', a.isnumeric())
-print('É um texto?', a.isalpha())
-print('É em somente em maiúsculas?', a.isupper())
-print('É somente em minúsculas?', a.islower())
-print('É capitalizado?', a.istitle())
-print('É alfanumérico?', a.isalnum())
-print('Está somente com espaço?', a.isspace())
-#  o trecho abaixo não faz parte do exercício, foi apenas um teste a mais.
-print('É um ASCII, (texto alfabetico sem ser ideogramas ou escrita arábica ou acentuado)?', a.isascii())
-print('É um dígito?', a.isdigit())
-print('É sem caracteres especiais? ',a.isidentifier())
-print('É imprimível?', a.isprintable())
-print('Bem-vindo {}, como está? Espero que esteja bem!'.format(a))
+def analisar_caracteres():
+    '''Analisa os dados recebidos e avalia diversas características.'''
+    a = receber_caracteres()
+    print('O tipo primitivo desse valor é', type(a))
+    print('É um número?', a.isnumeric())
+    print('É um texto?', a.isalpha())
+    print('É em somente em maiúsculas?', a.isupper())
+    print('É somente em minúsculas?', a.islower())
+    print('É capitalizado?', a.istitle())
+    print('É alfanumérico?', a.isalnum())
+    print('Está somente com espaço?', a.isspace())
+    print('É um dígito?', a.isdigit())
+    print('É sem caracteres especiais? ',a.isidentifier())
+    print('É imprimível?', a.isprintable())
+    print('\nIsso é tudo, tenha um bom dia!')
+
+
+analisar_caracteres()
