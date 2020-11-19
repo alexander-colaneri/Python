8279e0583b3250347d5376ff7a1708a07bbe2d47
alexander-colaneri <alexcol@gmail.com>
2020-11-18 22:52:29 -0300

    Update ex027-primeiro-e-ultimo-nome-de-uma-pessoa.py
    Transformado em classe.

--- studies/curso_em_video/ex027-primeiro-e-ultimo-nome-de-uma-pessoa.py
+++ studies/curso_em_video/ex027-primeiro-e-ultimo-nome-de-uma-pessoa.py
@@ -1,8 +1,42 @@
-# * Primeiro e último nome de uma pessoa *
-# Enunciado: Faça um programa que leia o nome de uma pessoa, mostrando o primeiro e o segundo nomes separadamente.
-print()
-n = str(input('Digite seu nome completo: ')).strip().title().split()
-print('Seja bem vindo!')
-print(f'Seu primeiro nome é {n[0]} e seu último sobrenome é {n[-1]}.')
-# Quando fazemos referência aos itens da lista do primeiro ao último, usamos números positivos, e do último ao
-# primeiro, números negativos. Por isso o [-1] faz referência ao último item da lista.
+# Primeiro e Último Nome de Uma Pessoa.
+# Descrição: Faça um programa que leia o nome completo de uma pessoa, mostrando em
+# seguida o primeiro e o último nome separadamente.
+
+class AnalisadorDeNomes():
+    '''Indica separadamente qual é o primeiro e o último sobrenome no nome completo indicado.'''
+    
+    def _init__(self):
+        self.nome_completo = ''
+        self.nome = ''
+        self.sobrenome = ''
+
+    def iniciar(self):
+        '''Início do programa.'''
+        print(f'{" ANALISADOR DE NOMES COMPLETOS ":*^41}')
+        self.receber_nome_completo()
+        self.analisar_nome_completo()
+        self.mostrar_resultados()
+        print('\nTenha um bom dia!')
+
+    def receber_nome_completo(self):
+        '''Recebe o nome completo a ser analisado.'''
+        print('Digite o nome completo:')
+        self.nome_completo = input()
+        return None
+
+    def analisar_nome_completo(self):
+        '''Separa o nome e o último sobrenome do nome completo indicado.'''
+        nome_completo = self.nome_completo.title().strip().split()
+        self.nome = nome_completo[0]
+        self.sobrenome = nome_completo[-1]
+        return None
+
+    def mostrar_resultados(self):
+        '''Exibe o nome e o último sobrenome.'''
+        nome = self.nome
+        sobrenome = self.sobrenome
+        print(f'\nO primeiro nome é {nome} e o último sobrenome é {sobrenome}.')
+
+
+analisador = AnalisadorDeNomes()
+analisador.iniciar()
